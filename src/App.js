5c006b1d9011a66322f57f858f1f6881e8c1c132
Rizanoor <rizanoorfauzan123@gmail.com>
2023-07-18 12:33:24 +0700

    feat: migrasri and add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrive from './components/Arrive';
+import Clients from './components/Clients';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-     <h1 className='text-3xl font-bold underline'>Hello world</h1>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrive />
+      <Clients />
+      <AsideMenu />
+      <Footer />
     </>
   );
 }
