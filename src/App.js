be1d7c8cbaf8c7e26470cb0104b17a4cd87079d6
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-20 15:17:14 +0700

    feat: integrasi with JSON

--- src/App.js
+++ src/App.js
@@ -7,21 +7,16 @@ import Clients from './components/Clients';
 import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
 
+import axios from "axios";
+
 function App() {
   const [items, setItems] = React.useState([]);
 
   React.useEffect(() => {
     (async function () {
-      const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
-      headers: {
-          "Content-Type": "application/json",
-          "accept": "application/json",
-          "x-api-key": process.env.REACT_APP_APIKEY
-        }
-      });
-      console.log(response);
-      const { nodes } = await response.json();
-      setItems(nodes);
+      const response = await axios.get('https://jsonplaceholder.org/posts');
+      setItems(response.data);
+      console.log(response.data);
     })();
     
   }, []);
@@ -31,7 +26,7 @@ function App() {
       <Header />
       <Hero />
       <Browse />
-      <Arrive />
+      <Arrive items={items} key={items.id} />
       <Clients />
       <AsideMenu />
       <Footer />
